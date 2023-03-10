d11a2b331716a72d5453ffec45082d6fafb6ef1a
saiyadm99 <niloyaali53@gmail.com>
2023-03-10 21:35:55 +0600

    05 done

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -2,7 +2,6 @@ import './category-item.styles.scss'
 
 const CategoryItem = ({category}) => {
 	const {imageUrl, title} = category;
-	console.log('sd')
 
 	return (
 		<div className="category-container">
